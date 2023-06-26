4584a697ca138d22252031f307d0c2955590021c
Brendan Dagys <brendandagys@gmail.com>
2023-06-25 21:13:42 -0400

    Add CORS headers

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -14,10 +14,10 @@ async fn main() -> Result<(), Error> {
 async fn handler(request: Request) -> Result<Response<Body>, Error> {
     match request.body() {
         Body::Text(body) => match serde_json::from_str::<RequestBody>(body) {
-            Ok(body) => {
-                Ok(Response::builder()
-                    .body(Body::Text(format!("{}", body.message.to_uppercase())))?)
-            }
+            Ok(body) => Ok(Response::builder()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "OPTIONS, POST")
+                .body(Body::Text(format!("{}", body.message.to_uppercase())))?),
             Err(error) => Ok(Response::builder().body(Body::Text(format!("ERROR: {:?}", error)))?),
         },
         _ => panic!(),
