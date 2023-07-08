5b4321a440a4e292b89eaa91d39757a9fe1705f0
Brendan Dagys <brendandagys@gmail.com>
2023-07-07 21:35:04 -0400

    Refactor and simplify some code

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -15,7 +15,7 @@ async fn handler(request: Request) -> Result<Response<Body>, Error> {
                 "{:?}",
                 merge_sort(&split_string_into_vec_of_ints(body))
             )))?),
-        _ => panic!(),
+        _ => Ok(Response::new(Body::Empty)),
     }
 }
 
@@ -24,16 +24,9 @@ fn split_string_into_vec_of_ints(string: &str) -> Vec<i32> {
     string
         .split(',')
         .into_iter()
-        .filter_map(|c| {
-            let trimmed = c.trim();
-            if trimmed.len() > 0 {
-                return match trimmed.parse::<i32>() {
-                    Ok(i) => Some(i),
-                    Err(_) => None,
-                };
-            }
-
-            None
+        .filter_map(|c| match c.trim().parse::<i32>() {
+            Ok(i) => Some(i),
+            Err(_) => None,
         })
         .collect()
 }
